6037506b4523da5a1d8cd31162719cb24457e032
Himanshi Kalra <himanshikalra98@gmail.com>
2021-01-13 18:36:14 +0530

    Added a chart for fiction vs non-fiction comparison

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ df1 = bestsellers.groupby(["Year"])["Reviews", "User Rating"].mean().reset_index
 fig1 = px.scatter(df1, x="Year", y="Reviews", color="User Rating",
                  title="Number of Amazon Reviews by Year with Average User Rating")
 
+
 #5 most reviewed authors
 df2 = bestsellers[["Author", "Reviews"]]
 df2 = df2.groupby(["Author"]).sum(["Reviews"]).reset_index()
@@ -23,11 +24,33 @@ df2 = df2.sort_values(by=['Reviews'], ascending=False)
 df2 = df2.head(5)
 fig2 = px.bar(df2, x='Author', y='Reviews', title="Number of Reviews of 5 Most Reviewed Authors")
 
+
+sorted_data = bestsellers.sort_values(['Year']).reset_index(drop=True)
+fiction_books = sorted_data[sorted_data["Genre"]== "Fiction"].reset_index(drop=True)
+non_fiction_books = sorted_data[sorted_data["Genre"] == "Non Fiction"].reset_index(drop=True)
+books_count = pd.DataFrame(index = fiction_books["Year"].unique())
+books_count["Books per Year Fiction"] = fiction_books[["Name","Year"]].groupby(by=["Year"]).count()
+books_count["Books per Year Non-Fiction"] = non_fiction_books[["Name", "Year"]].groupby(by=["Year"]).count()
+
+fig3 = go.Figure(data=[go.Bar( 
+    name = 'Fiction', 
+    x = books_count.index, y = books_count['Books per Year Fiction']
+   ), 
+                       go.Bar( 
+    name = 'Non-Fiction', 
+    x = books_count.index, y = books_count['Books per Year Non-Fiction']
+   ) 
+]) 
+fig3.update_layout(title="Comparison of Fiction vs Non-Fiction bestsellers over the Years")
 app = dash.Dash(__name__)
 app.layout = html.Div([
     dcc.Graph(figure=fig1),
-    dcc.Graph(figure=fig2)
+    dcc.Graph(figure=fig2),
+
+	dcc.Graph(figure=fig3),
+    
 ])
+	
 
 
 app.run_server(debug=True)
